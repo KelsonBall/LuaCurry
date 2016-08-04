63d2b088fb3ec678f839a12cf46a964daec6daac
Kelson Ball <kelson_ball@selinc.com>
2016-08-04 08:32:25 -0600

    Added curry

---
+++ curry.lua
@@ -0,0 +1,48 @@
+function curry(func, params)        
+    return (function (...)
+        local args = params or {}        
+        if #args + #{...} == debug.getinfo(func).nparams then
+            local args = {table.unpack(args)}
+            for _,v in ipairs({...}) do
+                table.insert(args, v)
+            end
+            return func(table.unpack(args))
+        else
+            local args = {table.unpack(args)}
+            for _,v in ipairs({...}) do
+                table.insert(args, v)
+            end
+            return curry(func, args)
+        end
+    end)
+end
+
+add = curry(function (a, b)    
+  return a + b
+end)
+
+assert( add(1)(2)(3) == 6 )
+
+assert( add(1)(1)(1) == 3 )
+
+local add2 = add(2)
+
+assert( add2(3) == 5 ) -- 5
+
+assert( add2(4) == 6 ) -- 3
+
+sum = curry(function(a, group)
+    for _,v in ipairs(group) do
+      a = a + v
+    end
+    return a
+end)
+
+local sumWith0 = sum(0)
+
+assert( sumWith0({1, 2, 3}) == 6)
+
+assert( sumWith0({2, 3, 4}) == 9 )
+
+assert( sum(1)({2, 3}) == 6 )
+
